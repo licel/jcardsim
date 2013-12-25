851053c5358fbf388a0dd86ccc6216d4a65d430a
Mikhail Dudarev <jcardsim@licel.ru>
2013-12-25 12:58:32 +0400

    [+] HMAC + RFC TESTS

--- src/main/java/javacard/security/KeyBuilder.java
+++ src/main/java/javacard/security/KeyBuilder.java
@@ -217,6 +217,62 @@ public class KeyBuilder {
     public static final short LENGTH_AES_256 = 256;
 
     /**
+     * Key object which implements interface type KoreanSEEDKey with
+     * CLEAR_ON_RESET transient key data. This Key object implicitly performs a
+     * clearKey() on power on or card reset.
+     */
+    public static final byte TYPE_KOREAN_SEED_TRANSIENT_RESET=16;
+    /**
+     * Key object which implements interface type KoreanSEEDKey with
+     * CLEAR_ON_DESELECT transient key data. This Key object implicitly performs
+     * a clearKey() on power on or card reset.
+     */
+    public static final byte TYPE_KOREAN_SEED_TRANSIENT_DESELECT = 17;
+    /**
+     * Key object which implements interface type KoreanSEEDKey with persistent
+     * key data.
+     */
+    public static final byte TYPE_KOREAN_SEED = 18;
+    /**
+     * Key object which implements interface type HMACKey with CLEAR_ON_RESET
+     * transient key data. This Key object implicitly performs a clearKey() on
+     * power on or card reset. Note, there is no length constant associated with
+     * HMAC, since the specification states that the key can have any length.
+     */
+    public static final byte TYPE_HMAC_TRANSIENT_RESET = 19;
+    /**
+     * Key object which implements interface type HMACKey with CLEAR_ON_DESELECT
+     * transient key data. This Key object implicitly performs a clearKey() on
+     * power on or card reset.
+     */
+    public static final byte TYPE_HMAC_TRANSIENT_DESELECT = 20;
+    /**
+     * Key object which implements interface type HMACKey with persistent key
+     * data.
+     */
+    public static final byte TYPE_HMAC = 21;
+    /**
+     * HMAC Key Length LENGTH_HMAC_SHA_1_BLOCK_64 = 64.
+     */
+    public static final short LENGTH_HMAC_SHA_1_BLOCK_64 = 64;
+    /**
+     * HMAC Key Length LENGTH_HMAC_SHA_256_BLOCK_64 = 64.
+     */
+    public static final short LENGTH_HMAC_SHA_256_BLOCK_64 = 64;
+    /**
+     * HMAC Key Length LENGTH_HMAC_SHA_384_BLOCK_128 = 64.
+     */
+    public static final short LENGTH_HMAC_SHA_384_BLOCK_128 = 128;
+    /**
+     * HMAC Key Length LENGTH_HMAC_SHA_512_BLOCK_128 = 64.
+     */
+    public static final short LENGTH_HMAC_SHA_512_BLOCK_128 = 128;
+    /**
+     * Korean Seed Key Length LENGTH_KOREAN_SEED_128 = 128.
+     */
+    public static final short LENGTH_KOREAN_SEED_128 = 128;
+
+    /**
      * Creates uninitialized cryptographic keys for signature and cipher algorithms. Only instances created
      * by this method may be the key objects used to initialize instances of
      * <code>Signature</code>, <code>Cipher</code> and <code>KeyPair</code>.
@@ -308,6 +364,13 @@ public class KeyBuilder {
                 key = new SymmetricKeyImpl(keyType, keyLength);
                 break;
 
+            // hmac
+            case TYPE_HMAC_TRANSIENT_RESET:
+            case TYPE_HMAC_TRANSIENT_DESELECT:
+            case TYPE_HMAC:
+                key = new SymmetricKeyImpl(keyType, keyLength);
+                break;
+                
             default:
                 CryptoException.throwIt(CryptoException.NO_SUCH_ALGORITHM);
                 break;
