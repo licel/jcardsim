99b0d16920eb0d256a98a654c2b02c7adf175978
dudarev <dudarev@licel.ru>
2015-11-25 14:42:25 +0300

    Fixed bug associated with incorrect generation of a proxy-class. Redundant console output was removed.

--- src/main/java/com/licel/jcardsim/utils/JavaCardApiProcessor.java
+++ src/main/java/com/licel/jcardsim/utils/JavaCardApiProcessor.java
@@ -61,7 +61,7 @@ public class JavaCardApiProcessor {
         proxyClass(buildDir, "com.licel.jcardsim.crypto.ChecksumProxy", "javacard.security.Checksum",true);
         proxyClass(buildDir, "com.licel.jcardsim.crypto.CipherProxy", "javacardx.crypto.Cipher",true);
         proxyClass(buildDir, "com.licel.jcardsim.crypto.KeyAgreementProxy", "javacard.security.KeyAgreement",true);
-        proxyClass(buildDir, "com.licel.jcardsim.crypto.KeyPairImpl", "javacard.security.KeyPair",false);
+        proxyClass(buildDir, "com.licel.jcardsim.crypto.KeyPairProxy", "javacard.security.KeyPair",false);
         proxyClass(buildDir, "com.licel.jcardsim.crypto.KeyBuilderProxy", "javacard.security.KeyBuilder",true);
         proxyClass(buildDir, "com.licel.jcardsim.crypto.MessageDigestProxy", "javacard.security.MessageDigest",true);
         proxyClass(buildDir, "com.licel.jcardsim.crypto.RandomDataProxy", "javacard.security.RandomData",true);
