1fbd556ef7af0d497d497d3909ce2bb1b96383b0
petrs <petrsgit@gmail.com>
2017-07-02 12:15:51 +0200

    added resize of ByteContainer if the new required length is bigger than existing one

--- src/test/java/com/licel/jcardsim/crypto/ByteContainerTest.java
+++ src/test/java/com/licel/jcardsim/crypto/ByteContainerTest.java
@@ -3,6 +3,7 @@ package com.licel.jcardsim.crypto;
 import junit.framework.TestCase;
 
 import java.math.BigInteger;
+import javacard.framework.JCSystem;
 
 public class ByteContainerTest extends TestCase {
     public ByteContainerTest(String name) {
@@ -52,4 +53,23 @@ public class ByteContainerTest extends TestCase {
 
         assertEquals(expected, new ByteContainer(expected).getBigInteger());
     }
+    
+    public void testSetBytes() {
+        short TEST_DATA_LEN = (short) 32;
+        byte[] expected = new byte[TEST_DATA_LEN];
+        ByteContainer container = new ByteContainer(expected, (short) 0, (short) expected.length);
+        byte[] dataResult = container.getBytes(JCSystem.MEMORY_TYPE_TRANSIENT_RESET);
+        assertEquals(expected.length, dataResult.length);
+        
+        // Try to set with shorter and longer array - length should change accordingly
+        byte[] shorter = new byte[(short) (TEST_DATA_LEN - 2)];
+        container.setBytes(shorter);
+        dataResult = container.getBytes(JCSystem.MEMORY_TYPE_TRANSIENT_RESET);
+        assertEquals(shorter.length, dataResult.length);
+        byte[] longer = new byte[(short) (TEST_DATA_LEN + 2)];
+        container.setBytes(longer);
+        dataResult = container.getBytes(JCSystem.MEMORY_TYPE_TRANSIENT_RESET);
+        assertEquals(longer.length, dataResult.length);
+    }
+    
 }
