460c8c9703271c7968f2afd1b37a19a5ca25236a
peroksid <peroksid90@gmail.com>
2020-10-21 15:41:26 +0100

    fix comment Licel LLC - > Licel Corporation

--- src/main/java/com/licel/jcardsim/base/PersistentSimulatorRuntime.java
+++ src/main/java/com/licel/jcardsim/base/PersistentSimulatorRuntime.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Licel LLC.
+ * Copyright 2020 Licel Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
